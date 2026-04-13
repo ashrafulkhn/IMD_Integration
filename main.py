0648493752fd46af2d4bd0026de5332655d63755
meghanamaha4 <meghanamaha4@gmail.com>
2026-04-13 14:41:24 +0530

    Descreasing loading time and few modifications

--- main.py
+++ main.py
@@ -18,62 +18,64 @@ def main() -> None:
     # port = "COM11"  # for windows
     port = "/dev/ttymxc1"  # for linux
 
-    client = IMDClient(port="com11",
+    client = IMDClient(port="/dev/ttymxc1",  #com11 using it is for windows
                        baudrate=9600,
                        parity="N",
                        stopbits=1,
                        bytesize=8,
-                       timeout=2.0)   
+                       timeout=0.3)   
 
     if not client.connect():
-        print(f"Failed to connect to IMD on port {port}")
+        print("connection failed")
         return
     
     
     # comment out the following lines if you want to test with only one IMD connected (e.g. just address 1)
 
-    try:
-        while True:
-            # for i in range(3): 
-            print("--------------------------------")
-            print(f"Attempt to enable insulation monitoring")
-            print("--------------------------------")
+    # try:
+    #     while True:
+    #         # for i in range(3): 
+    #         # print("--------------------------------")
+    #         print(end= " ")
+    #         print("Attempt to enable insulation monitoring")
+    #         print("--------------------------------")
 
             #  Enable insulation monitoring on both channels
             
-            status_1 = client.read_channel_status(IMD_1_ADDRESS)   
+            # status_1 = client.read_channel_status(IMD_1_ADDRESS)   
             
-            register_1 = client._write_single_register(IMD_1_ADDRESS , client.REG_INSULATION_CONTROL, 0x0011)
-            register_2 = client._write_single_register(IMD_2_ADDRESS , client.REG_INSULATION_CONTROL, 0x0012)
+    register_1 = client._write_single_register(IMD_1_ADDRESS , client.REG_INSULATION_CONTROL, 0x0011)
+    register_2 = client._write_single_register(IMD_2_ADDRESS , client.REG_INSULATION_CONTROL, 0x0012)
                         
-            enabled_ch1 = client.enable_insulation_monitoring(IMD_1_ADDRESS)
-            enabled_ch2 = client.enable_insulation_monitoring(IMD_2_ADDRESS)
-         
-            print(f"Enable CH1 monitoring: {'success' if enabled_ch1 else 'failed'}")
-            print(f"Enable CH2 monitoring: {'success' if enabled_ch2 else 'failed'}")
+    enabled_ch1 = client.enable_insulation_monitoring(IMD_1_ADDRESS)
+    enabled_ch2 = client.enable_insulation_monitoring(IMD_2_ADDRESS)
+                                                                                                                                                                
+            # print(f"Enable CH1 monitoring: {'success' if enabled_ch1 else 'failed'}")
+            # print(f"Enable CH2 monitoring: {'success' if enabled_ch2 else 'failed'}")
 
-            # Read full status for IMD1 (address 1) and IMD2 (address 2)
-            print("\nIMD1 status:")
+    try:
+        while True:# Read full status for IMD1 (address 1) and IMD2 (address 2)
+            print("========== IMD1 status: ==========")
             imd1_status = client.IMD1StatusRequest(unit_id=IMD_1_ADDRESS)
             pprint(imd1_status)
 
-            print("\nIMD2 status:")
+            print("========== IMD2 status: ==========")
             imd2_status = client.IMD2StatusRequest(unit_id=IMD_2_ADDRESS)
             pprint(imd2_status)
 
-            time.sleep(1)
+            time.sleep(0.2)
 
         # Example: disable monitoring again
             disabled_ch1 = client.disable_insulation_monitoring(IMD_1_ADDRESS)
-       
             disabled_ch2 = client.disable_insulation_monitoring(IMD_2_ADDRESS)
-            print(f"\nDisable CH1 monitoring: {'success' if disabled_ch1 else 'failed'}")
-            print(f"Disable CH2 monitoring: {'success' if disabled_ch2 else 'failed'}")
+            
+            # print(f"\nDisable CH1 monitoring: {'success' if disabled_ch1 else 'failed'}")
+            # print(f"Disable CH2 monitoring: {'success' if disabled_ch2 else 'failed'}")
  
     finally:
         client.close()
         
-    
+                   
 
 if __name__ == "__main__":
     main()
